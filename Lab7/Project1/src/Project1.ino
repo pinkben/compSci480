6f630f62408cb46c39dfb739b0a87650d59dcde0
Ben Pink <pinkb95@uwosh.edu>
2022-03-16 14:19:10 -0500

    lab7 starter

---
+++ Lab7/Project1/src/Project1.ino
@@ -0,0 +1,68 @@
+/**** 
+Name: Lab7/Project1 
+Author: Ben Pink
+Description: Your Description Here 
+Bugs: A list here (or "none that I know of", if that is the case) 
+Reflection: A short discussion of how you found this assignment 
+****/ 
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+int buttonA = A2;
+int buttonB = D2;
+int aButtonTimer;
+int bButtonTimer;
+
+Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
+
+void setup() {
+  Serial.begin(9600);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  
+  display.display();
+  delay(1000);
+  display.clearDisplay();
+  display.display();
+  aButtonTimer = Time.now(); 
+  bButtonTimer = Time.now();
+}
+
+
+void loop() {
+  // Button A pressed
+  if (digitalRead(buttonA) == LOW && digitalRead(buttonB) != LOW)
+  {
+    if(Time.now() - aButtonTimer < 1)
+    {
+      aButtonTimer--;
+    }
+  }
+  // Button B Pressed
+  else if (digitalRead(buttonB) == LOW && digitalRead(buttonA) != LOW)
+  {
+    if(Time.now() - bButtonTimer < 1)
+    {
+      bButtonTimer--;
+    }
+  }
+  // Both buttons pressed
+  else if (digitalRead(buttonA) == LOW && digitalRead(buttonB) == LOW)
+  {
+    if((Time.now() - aButtonTimer < 1) && (Time.now() - bButtonTimer < 1))
+    {
+      aButtonTimer--;
+      bButtonTimer--;
+    }
+
+  }
+  // No buttons pressed
+  else
+  {
+    aButtonTimer = Time.now();
+    bButtonTimer = Time.now();
+  }
+
+}
---
+++ Lab7/Project1/target/2.3.0/argon/obj/src/module_info.o.d
@@ -0,0 +1,48 @@
+/Users/pinkb95/Desktop/CS480/compSci480/Lab7/Project1/target/2.3.0/argon//obj/./src/module_info.o: \
+ src/module_info.c ../../../dynalib/inc/module_info.inc \
+ ../../../dynalib/inc/module_info.h ../../../services/inc/static_assert.h \
+ /Users/pinkb95/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/assert.h \
+ /Users/pinkb95/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_ansi.h \
+ /Users/pinkb95/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/newlib-nano/newlib.h \
+ /Users/pinkb95/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_newlib_version.h \
+ /Users/pinkb95/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/config.h \
+ /Users/pinkb95/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/ieeefp.h \
+ /Users/pinkb95/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/features.h \
+ /Users/pinkb95/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h \
+ /Users/pinkb95/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /Users/pinkb95/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/stdint.h \
+ /Users/pinkb95/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/_default_types.h \
+ /Users/pinkb95/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_intsup.h \
+ /Users/pinkb95/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_stdint.h
+
+../../../dynalib/inc/module_info.inc:
+
+../../../dynalib/inc/module_info.h:
+
+../../../services/inc/static_assert.h:
+
+/Users/pinkb95/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/assert.h:
+
+/Users/pinkb95/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_ansi.h:
+
+/Users/pinkb95/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/newlib-nano/newlib.h:
+
+/Users/pinkb95/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_newlib_version.h:
+
+/Users/pinkb95/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/config.h:
+
+/Users/pinkb95/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/ieeefp.h:
+
+/Users/pinkb95/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/features.h:
+
+/Users/pinkb95/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h:
+
+/Users/pinkb95/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h:
+
+/Users/pinkb95/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/stdint.h:
+
+/Users/pinkb95/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/_default_types.h:
+
+/Users/pinkb95/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_intsup.h:
+
+/Users/pinkb95/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_stdint.h:
